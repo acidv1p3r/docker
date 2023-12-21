597d5fd37e5005ba59931ca501136bc21c5a8667
Greelan <53196309+Greelan@users.noreply.github.com>
2023-12-21 22:16:26 +1100

    Updates

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker build --rm -f AMPDockerFileUbuntu -t greelan/amp:ubuntu .
-docker build --rm -f AMPDockerFileWineStaging -t greelan/amp:wine-staging .
-docker build --rm -f AMPDockerFileSinusBot -t greelan/amp:sinusbot .
+docker build --rm -f AMPDockerfileUbuntu -t greelan/amp:ubuntu .
+docker build --rm -f AMPDockerfileWineStaging -t greelan/amp:wine-staging .
+docker build --rm -f AMPDockerfileSinusBot -t greelan/amp:sinusbot .
 
 docker push -a greelan/amp
