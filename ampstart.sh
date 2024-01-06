ae27343fc307826288321abbb966108b0aa39941
Greelan <53196309+Greelan@users.noreply.github.com>
2024-01-06 11:33:40 +1100

    DockerFile updates

--- ampstart.sh
+++ ampstart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "[Info] AMPStart for Docker - v22.12.1"
+echo "[Info] AMPStart for Docker - v23.07.1"
+INSTALLED_DEPS_FILE="/AMP/InstalledDeps.json"
 
 if [ -z "${AMPUSERID}" ]; then
   echo "[Info] This docker image cannot be used directly by itself - it must be started by ampinstmgr"
@@ -19,10 +20,47 @@ else
     touch /home/amp/.gitconfig
     chown -R amp:amp /home/amp 2> /dev/null
     usermod -aG tty amp
-    chmod +x /AMP/AMP_Linux_x86_64
+    chmod +x /AMP/AMP_Linux_aarch64
     echo "[Info] Container setup complete."
 fi
 
+if [ -f "$INSTALLED_DEPS_FILE" ]; then
+  INSTALLED_DEPS=$(jq -r '.[]' $INSTALLED_DEPS_FILE)
+else
+  INSTALLED_DEPS=""
+  echo "[]" > $INSTALLED_DEPS_FILE
+fi
+
+REQUIRED_DEPS=$(echo $AMP_CONTAINER_DEPS | jq -r '.[]')
+
+DEPS_TO_INSTALL=()
+for DEP in $REQUIRED_DEPS; do
+  if ! [[ $INSTALLED_DEPS =~ $DEP ]]; then
+    DEPS_TO_INSTALL+=($DEP)
+  fi
+done
+
+if [ ${#DEPS_TO_INSTALL[@]} -ne 0 ]; then
+  echo "[Info] Installing dependencies..."
+  apt-get update
+  apt-get install -y ${DEPS_TO_INSTALL[@]}
+
+#  for DEP in ${DEPS_TO_INSTALL[@]}; do
+#    jq --arg dep "$DEP" '. += [$dep]' $INSTALLED_DEPS_FILE > temp && mv temp $INSTALLED_DEPS_FILE
+#  done
+
+  apt-get clean
+  rm -rf /var/lib/apt/lists/*
+  echo "[Info] Installation complete."
+else
+  echo "[Info] No missing dependencies to install."
+fi
+
+export AMPHOSTPLATFORM
+export AMP_CONTAINER
+export AMPMEMORYLIMIT
+
 ARGS=$@
-exec su -l -c "ampinstmgr --sync-certs; cd /AMP; HOME=/home/amp /AMP/AMP_Linux_x86_64 ${ARGS}; exit $?" -- amp
+exec su -l -w AMPHOSTPLATFORM,AMP_CONTAINER,AMPMEMORYLIMIT -c "ampinstmgr --sync-certs; cd /AMP; HOME=/home/amp /AMP/AMP_Linux_aarch64 ${ARGS}; exit $?" -- amp
 exit $?
+
