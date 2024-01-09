cb03d5d55f07890ad2db01500d757038932aa659
Greelan <53196309+Greelan@users.noreply.github.com>
2024-01-09 11:49:38 +1100

    Update buildDockerBase.sh

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker build --rm --pull --no-cache -f AMPDockerfileUbuntu -t greelan/amp:ubuntu .
-docker build --rm --pull --no-cache -f AMPDockerfileWineStaging -t greelan/amp:wine-staging .
-docker build --rm --pull --no-cache -f AMPDockerfileSinusBot -t greelan/amp:sinusbot .
+#docker build --rm --pull --no-cache -f AMPDockerfileWineStaging -t greelan/amp:wine-staging .
+#docker build --rm --pull --no-cache -f AMPDockerfileSinusBot -t greelan/amp:sinusbot .
 docker build --rm --pull --no-cache -f AMPDockerfileAMPBaseARM -t greelan/amp:arm .
 docker build --rm --pull --no-cache -f AMPDockerfileAMPBaseARMJava -t greelan/amp:arm-java .
 docker build --rm --pull --no-cache -f AMPDockerfileAMPBaseARMPython3 -t greelan/amp:arm-python3 .
