ce3795d0f78e01dad3618d0425139bc3b64c3b62
Greelan <53196309+Greelan@users.noreply.github.com>
2024-07-27 03:46:36 +1000

    Update buildDockerBase.sh

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -5,5 +5,6 @@ docker build --rm --pull --no-cache -f AMPDockerfileUbuntu -t greelan/amp:ubuntu
 docker build --rm --pull --no-cache -f AMPDockerfileAMPBaseARM -t greelan/amp:arm .
 docker build --rm --pull --no-cache -f AMPDockerfileAMPBaseARMJava -t greelan/amp:arm-java .
 docker build --rm --pull --no-cache -f AMPDockerfileAMPBaseARMPython3 -t greelan/amp:arm-python3 .
+docker build --rm --pull --no-cache -f AMPDockerfileAMPBaseARMCPx -t greelan/amp:arm-cpx .
 
 docker push -a greelan/amp
