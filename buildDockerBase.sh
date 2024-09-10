d849c1a469847e2ba5b1682530f7f1c6c2aa1fc7
Greelan <53196309+Greelan@users.noreply.github.com>
2024-09-10 22:55:00 +1000

    Update buildDockerBase.sh

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+docker image prune -f
+
 docker build --rm --pull --no-cache --progress=plain -f AMPDockerfileUbuntu -t greelan/amp:ubuntu .
 #docker build --rm --pull --no-cache -f AMPDockerfileWineStaging -t greelan/amp:wine-staging .
 #docker build --rm --pull --no-cache -f AMPDockerfileSinusBot -t greelan/amp:sinusbot .
