00fc859ec4cd6a63301cd2e94330a709780f3a86
Greelan <53196309+Greelan@users.noreply.github.com>
2024-10-25 18:35:04 +1100

    Updates

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 docker image prune -f
 
-docker build --rm --pull --no-cache --progress=plain -f AMPDockerfileUbuntu -t greelan/amp:ubuntu .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/amd64 -f AMPDockerfileUbuntu -t greelan/amp:ubuntu .
 #docker build --rm --pull --no-cache -f AMPDockerfileWineStaging -t greelan/amp:wine-staging .
 #docker build --rm --pull --no-cache -f AMPDockerfileSinusBot -t greelan/amp:sinusbot .
-docker build --rm --pull --no-cache --progress=plain -f AMPDockerfileAMPBaseARM -t greelan/amp:arm .
-docker build --rm --pull --no-cache --progress=plain -f AMPDockerfileAMPBaseARMJava -t greelan/amp:arm-java .
-docker build --rm --pull --no-cache --progress=plain -f AMPDockerfileAMPBaseARMPython3 -t greelan/amp:arm-python3 .
-docker build --rm --pull --no-cache --progress=plain -f AMPDockerfileAMPBaseARMCPx -t greelan/amp:arm-cpx .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileAMPBaseARM -t greelan/amp:arm .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileAMPBaseARMJava -t greelan/amp:arm-java .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileAMPBaseARMPython3 -t greelan/amp:arm-python3 .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileAMPBaseARMCPx -t greelan/amp:arm-cpx .
 
 docker push -a greelan/amp
