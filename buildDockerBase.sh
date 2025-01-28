ba1c8dd4afaed8ac081eebae75dafec4908791a6
Acidv1p3r <50040279+acidv1p3r@users.noreply.github.com>
2025-01-28 13:00:19 -0500

    Update buildDockerBase.sh

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 docker image prune -f
 
-docker build --rm --pull --no-cache --progress=plain --platform=linux/amd64 -f AMPDockerfileUbuntu -t greelan/amp:ubuntu .
-docker build --rm --pull --no-cache --progress=plain --platform=linux/amd64 -f AMPDockerfileUptimeKuma2 -t greelan/amp:uptimekuma2 .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/amd64 -f AMPDockerfileUbuntu -t acidv1p3r/amp:ubuntu .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/amd64 -f AMPDockerfileUptimeKuma2 -t acidv1p3r/amp:uptimekuma2 .
 #docker build --rm --pull --no-cache -f AMPDockerfileWineStaging -t greelan/amp:wine-staging .
 #docker build --rm --pull --no-cache -f AMPDockerfileSinusBot -t greelan/amp:sinusbot .
-docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARM -t greelan/amp:arm .
-docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARMJava -t greelan/amp:arm-java .
-docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARMPython3 -t greelan/amp:arm-python3 .
-docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARMCPx -t greelan/amp:arm-cpx .
-docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARMCPxWine9 -t greelan/amp:arm-cpx-wine9 .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARM -t acidv1p3r/amp:arm .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARMJava -t acidv1p3r/amp:arm-java .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARMPython3 -t acidv1p3r/amp:arm-python3 .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARMCPx -t acidv1p3r/amp:arm-cpx .
+docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARMCPxWine9 -t acidv1p3r/amp:arm-cpx-wine9 .
 
-docker push -a greelan/amp
+docker push -a acidv1p3r/amp
