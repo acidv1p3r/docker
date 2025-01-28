599cfb3fbba9656a9d4cb71fc638a7a96bbcebed
Acidv1p3r <50040279+acidv1p3r@users.noreply.github.com>
2025-01-28 13:00:41 -0500

    Update buildDockerBase.sh

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -3,8 +3,8 @@ docker image prune -f
 
 docker build --rm --pull --no-cache --progress=plain --platform=linux/amd64 -f AMPDockerfileUbuntu -t acidv1p3r/amp:ubuntu .
 docker build --rm --pull --no-cache --progress=plain --platform=linux/amd64 -f AMPDockerfileUptimeKuma2 -t acidv1p3r/amp:uptimekuma2 .
-#docker build --rm --pull --no-cache -f AMPDockerfileWineStaging -t greelan/amp:wine-staging .
-#docker build --rm --pull --no-cache -f AMPDockerfileSinusBot -t greelan/amp:sinusbot .
+#docker build --rm --pull --no-cache -f AMPDockerfileWineStaging -t acidv1p3r/amp:wine-staging .
+#docker build --rm --pull --no-cache -f AMPDockerfileSinusBot -t acidv1p3r/amp:sinusbot .
 docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARM -t acidv1p3r/amp:arm .
 docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARMJava -t acidv1p3r/amp:arm-java .
 docker build --rm --pull --no-cache --progress=plain --platform=linux/arm64 -f AMPDockerfileARMPython3 -t acidv1p3r/amp:arm-python3 .
